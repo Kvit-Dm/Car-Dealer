11077ec1f56167473159d640edd7c245c7c80864
Anti-RakPro <kvit.d.o@gmail.com>
2024-10-30 13:38:10 +0200

    fix: before folder structure chendge

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
+import FetchCars from "@/app/components/FetchCars";
 
 export default function Home() {
+
+
+
     return (
         <React.Fragment>
+          <FetchCars/>
           <div>
             some text
           </div>
